65beb3172971559b1ce05ddbe7bfb7a00e17d1c9
muneebawan44 <143783326+muneebawan44@users.noreply.github.com>
2023-09-05 22:04:23 +0500

    Add files via upload

---
+++ five.js
@@ -0,0 +1,76 @@
+//Q1
+var studentName = [];
+studentName;
+
+//Q2
+var studentName = [];
+studentName;
+
+//Q3
+var breakfast = ['bread', 'Milk', 'Egg', 'paratha', 'Tea' ]
+breakfast;
+
+//Q4
+var number = [2,4,7,8,5,10]
+number;
+
+//Q5
+var boolean = [true,false]
+boolean;
+
+//Q6
+var mixed = ['podina',3,'bread',true,8,'dhaniya',false]
+mixed;
+
+//Q7
+var qualification = ['SSC', 'HSC', 'BCS', 'BS', 'BCOM', 'MS', 'M. Phil.', 'PhD'];
+qualification;
+
+//Q8
+
+var percentage1 = 344/500*100
+var percentage2 = 450/500*100
+var percentage3 = 420/500*100
+var studentName = ['Aliyan', 'Muneeb', 'Mubashir']
+var score = [344,450,420]
+//alert('Score of ' + studentName[0] + ' is ' + score[0] + '. percentage: ' + percentage1 + '%' + '\n Score of ' + studentName[1] + ' is ' + score[1] + '. percentage: ' + percentage2 + '%' + '\n Score of ' + studentName[2] + ' is ' + score[2] + '. percentage: ' + percentage2 + '%')
+
+//Q9
+
+
+
+
+
+//Q11
+
+var cities = ['Rawalpindi', 'Lahore', 'Karachi', 'Quetta', 'Peshawar']
+//alert('Cities list:\n' + cities + '\n \n \n \n Selected Cities list:\n' + cities[0] + ',' + cities[2])
+
+
+//Q12
+
+var arr = ['This', 'is', 'my', 'cat']
+//alert('Array: \n' + arr + '\n \n' + 'string: \n' + arr[0] + ' ' + arr[1] + ' '  + arr[2] + ' ' + arr[3])
+
+//Q13
+
+ var fifo = []
+  fifo.push("Keyboard")
+  fifo.push("Mouse")
+  fifo.push("Printer")
+  fifo.push("Monitor")
+  
+
+ //alert("Devices \n"+  fifo + "\n \n Out: \n"+fifo[0] + "\n" +  "Out: \n"+fifo[1] + "\n" + "Out: \n"+fifo[2] + "\n" + "Out: \n"+fifo[3]  )
+
+ //Q14
+
+
+ fifo.reverse()
+
+ //alert("Devices \n"+  fifo + "\n \n Out: \n"+fifo[0] + "\n" +  "Out: \n"+fifo[1] + "\n" + "Out: \n"+fifo[2] + "\n" + "Out: \n"+fifo[3]  )
+
+ //Q15
+
+ var Mob = ['Samsung','Nokia','Apple','Motorola','One+','Oppo','Vivo']
+//document.write(Mob)
